8c8c24a3b95b9e96b89ddfa0dbe22cb1621f30ab
Leeyulong <1097910340@qq.com>
2020-06-10 16:55:55 +0800

    param

--- src/views/modules/sys/param.vue
+++ src/views/modules/sys/param.vue
@@ -1,90 +1,219 @@
 <template>
-    <div class="app-container">
-        <div class="filter-container">
-            <el-input v-model="listQuery.roleName" placeholder="角色名" style="width: 200px;" class="filter-item"
-                clearable />
-            <el-button class="filter-item" type="primary" icon="el-icon-search" @click="getDataList">
-                搜索
-            </el-button>
-            <el-button v-if="isAuth('sys:role:save')" class="filter-item" style="margin-left: 10px;" type="primary"
-                icon="el-icon-plus" @click="handleCreate">
-                添加
-            </el-button>
-            <el-button v-if="isAuth('sys:role:update')" class="filter-item" style="margin-left: 10px;" type="primary"
-                icon="el-icon-edit" @click="handleEdit">
-                修改
-            </el-button>
-            <el-button v-if="isAuth('sys:role:delete')" class="filter-item" style="margin-left: 10px;" type="danger"
-                icon="el-icon-delete" @click="handleDelete">
-                删除
-            </el-button>
-        </div>
-
-        <el-table :key="tableKey" v-loading="listLoading" :data="dataList" border fit highlight-current-row
-            style="width: 100%;">
-            <el-table-column type="index" width="50">
-            </el-table-column>
-            <el-table-column type="selection" width="55" align="center"></el-table-column>
-            <el-table-column prop="menuUrl" label="参数url">
-            </el-table-column>
-            <el-table-column prop="parKey" label="参数键" width="180">
-            </el-table-column>
-            <el-table-column prop="parValue" label="参数值" width="180">
-            </el-table-column>
-            <el-table-column prop="type" label="参数类型">
-            </el-table-column>
-        </el-table>
-
-        <pagination v-show="listQuery.total>0" :total="listQuery.total" :page.sync="listQuery.page"
-            :limit.sync="listQuery.size" @pagination="getDataList" />
+    <div class="mod-config">
+        <el-row>
+            <el-col :span="9">
+                <el-card>
+                    <div slot="header">
+                        <span>菜单列表</span>
+                    </div>
+                    <div>
+                        <el-tree :data="menuList" :props="menuListTreeProps" node-key="menuId" ref="menuListTree"
+                            @current-change="menuListTreeCurrentChangeHandle" :default-expand-all="true" :highlight-current="true"
+                            :expand-on-click-node="false">
+                        </el-tree>
+                    </div>
+                </el-card>
+            </el-col>
+            <el-col :span="15">
+                <el-card>
+                    <div slot="header">
+                        <span>参数列表</span>
+                    </div>
+                    <div>
+                        <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+                            <el-form-item>
+                                <el-input v-model="dataForm.type" placeholder="类型" clearable></el-input>
+                            </el-form-item>
+                            <el-form-item>
+                                <el-button @click="getDataList()" icon="el-icon-search" type="primary">查询</el-button>
+                                <el-button v-if="isAuth('sys:param:save')" type="primary" icon="el-icon-plus"
+                                    @click="addOrUpdateHandle()">
+                                    新增</el-button>
+                                <el-button v-if="isAuth('sys:param:delete')" type="danger" @click="deleteHandle()"
+                                    :disabled="dataListSelections.length <= 0">批量删除</el-button>
+                            </el-form-item>
+                        </el-form>
+                        <el-table :data="dataList" border v-loading="dataListLoading" @selection-change="selectionChangeHandle"
+                            style="width: 100%;">
+                            <el-table-column type="selection" header-align="center" align="center" width="50">
+                            </el-table-column>
+                            <el-table-column prop="parKey" header-align="center" align="center" label="参数键">
+                            </el-table-column>
+                            <el-table-column prop="parValue" header-align="center" align="center" label="参数值">
+                            </el-table-column>
+                            <el-table-column prop="menuUrl" header-align="center" align="center" label="参数url">
+                            </el-table-column>
+                            <el-table-column prop="type" header-align="center" align="center" label="参数类型">
+                            </el-table-column>
+                            <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
+                                <template slot-scope="scope">
+                                    <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改
+                                    </el-button>
+                                    <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">删除
+                                    </el-button>
+                                </template>
+                            </el-table-column>
+                        </el-table>
+                        <el-pagination @size-change="sizeChangeHandle" @current-change="currentChangeHandle"
+                            :current-page="pageIndex" :page-sizes="[10, 20, 50, 100]" :page-size="pageSize" :total="totalPage"
+                            layout="total, sizes, prev, pager, next, jumper">
+                        </el-pagination>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+        <!-- 弹窗, 新增 / 修改 -->
+        <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
     </div>
 </template>
 
 <script>
-import { timeformat } from "@/utils/util";
-import Pagination from "@/components/Pagination"; // secondary package based on el-pagination
+import AddOrUpdate from "./paramAddOrUpdate";
+import { treeDataTranslate } from "@/utils/util";
 export default {
     name: "sys-param",
-    components: { Pagination },
     data() {
         return {
-            dataList: [],
-            listQuery: {
-                page: 1,
-                size: 10,
-                total: 0,
-                roleName: ""
+            dataForm: {
+                type: "",
+                menuUrl: ""
             },
-            listLoading: false,
-            tableKey: 0
+            dataList: [],
+            pageIndex: 1,
+            pageSize: 10,
+            totalPage: 0,
+            dataListLoading: false,
+            dataListSelections: [],
+            addOrUpdateVisible: false,
+            menuList: [],
+            menuListTreeProps: {
+                label: "name",
+                children: "children"
+            },
+            dynamicMenuRoutes: JSON.parse(
+                sessionStorage.getItem("dynamicMenuRoutes") || "[]"
+            )
         };
     },
-    created() {
+    components: {
+        AddOrUpdate
+    },
+    activated() {
         this.getDataList();
+        this.initMenu();
     },
     methods: {
+        // 获取数据列表
         getDataList() {
-            this.listLoading = true;
+            this.dataListLoading = true;
             this.$http({
                 url: "/admin/sys/param/list",
                 method: "get",
-                params: this.$http.adornParams(this.listQuery)
+                params: this.$http.adornParams({
+                    page: this.pageIndex,
+                    limit: this.pageSize,
+                    menuUrl: this.dataForm.menuUrl,
+                    type: this.dataForm.type
+                })
             }).then(({ data }) => {
-                if (data.code === 2000) {
+                if (data && data.code === 2000) {
                     this.dataList = data.data.list;
-                    this.listQuery.total = data.data.totalCount;
+                    this.totalPage = data.data.totalCount;
                 } else {
-                    this.$myNotify.error(data.message);
+                    this.dataList = [];
+                    this.totalPage = 0;
                 }
-                this.listLoading = false;
+                this.dataListLoading = false;
             });
         },
-        handleCreate() {},
-        handleEdit() {},
-        handleDelete() {},
-        dateformat(row) {
-            return timeformat(row.createTime);
+        // 每页数
+        sizeChangeHandle(val) {
+            this.pageSize = val;
+            this.pageIndex = 1;
+            this.getDataList();
+        },
+        // 当前页
+        currentChangeHandle(val) {
+            this.pageIndex = val;
+            this.getDataList();
+        },
+        // 多选
+        selectionChangeHandle(val) {
+            this.dataListSelections = val;
+        },
+        // 新增 / 修改
+        addOrUpdateHandle(id) {
+            this.addOrUpdateVisible = true;
+            this.$nextTick(() => {
+                this.$refs.addOrUpdate.init(id, this.dataForm.menuUrl);
+            });
+        },
+        // 删除
+        deleteHandle(id) {
+            var ids = id
+                ? [id]
+                : this.dataListSelections.map(item => {
+                    return item.id;
+                });
+            this.$confirm(
+                `确定对这${ids.length}条数据进行[${
+                id ? "删除" : "批量删除"
+                }]操作?`,
+                "提示",
+                {
+                    confirmButtonText: "确定",
+                    cancelButtonText: "取消",
+                    type: "warning"
+                }
+            ).then(() => {
+                this.$http({
+                    url: "/admin/sys/param/delete",
+                    method: "delete",
+                    data: this.$http.adornData(ids, false)
+                }).then(({ data }) => {
+                    if (data && data.code === 2000) {
+                        this.$message({
+                            message: "操作成功",
+                            type: "success",
+                            duration: 1000,
+                            onClose: () => {
+                                this.getDataList();
+                            }
+                        });
+                    } else {
+                        this.$message.error(data.message);
+                    }
+                });
+            });
+        },
+        // 初始化菜单
+        initMenu() {
+            this.$http({
+                url: "/admin/sys/menu/select",
+                method: "get",
+                params: this.$http.adornParams()
+            }).then(({ data }) => {
+                this.menuList = treeDataTranslate(data.data, "menuId");
+            });
+        },
+        // 菜单树选中
+        menuListTreeCurrentChangeHandle(data, node) {
+            let route = this.dynamicMenuRoutes.filter(
+                item => item.meta.menuId === data.menuId
+            );
+            if (route.length >= 1) {
+                this.dataForm.menuUrl = "/" + route[0].path;
+            } else {
+                this.dataForm.menuUrl = "";
+            }
+            this.getDataList();
         }
     }
 };
 </script>
+
+<style scoped>
+.mod-config {
+    margin: 20px;
+}
+</style>
