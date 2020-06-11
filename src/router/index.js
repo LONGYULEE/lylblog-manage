454f859c2e463e1947e9b801671f5843c5f6eca1
Leeyulong <1097910340@qq.com>
2020-06-11 16:24:21 +0800

    article

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from '@/layout';
 
 //开发环境不使用懒加载，生产环境下使用
 const _import = require('./_import_' + process.env.NODE_ENV);
 
-//不需要权限通用路由表 
+//不需要权限通用路由表
 //如首页和登录页和一些不用权限的公用页面
 export const constantRoutes = [
     {
@@ -22,7 +22,12 @@ export const constantRoutes = [
             }
         ]
     },
-    { path: '/login', component: _import('common/login'), name: 'login', meta: { title: '登录' } },
+    {
+        path: '/login',
+        component: _import('common/login'),
+        name: 'login',
+        meta: { title: '登录' }
+    },
     { path: '/', redirect: '/login' },
     {
         path: '/home',
@@ -41,6 +46,15 @@ export const constantRoutes = [
                 hidden: true
             },
             {
+                path: '/article/article/update/:id',
+                component: _import('modules/article/articleAddOrUpdate'),
+                name: 'article-update',
+                meta: {
+                    menuId: 'article-update',
+                    title: '博文修改'
+                }
+            },
+            {
                 path: '/404',
                 component: _import('common/404'),
                 name: '404',
@@ -49,23 +63,24 @@ export const constantRoutes = [
             }
         ]
     }
-]
+];
 
-export const asyncRoutes = []
+export const asyncRoutes = [];
 
-const createRouter = () => new Router({
-    // mode: 'history', // require service support
-    scrollBehavior: () => ({ y: 0 }),
-    routes: constantRoutes
-})
+const createRouter = () =>
+    new Router({
+        // mode: 'history', // require service support
+        scrollBehavior: () => ({ y: 0 }),
+        routes: constantRoutes
+    });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 // 创建一个船新的router对象，解决router.addRouters()退出时没有清理
 export function resetRouter() {
-    const newRouter = createRouter()
-    router.matcher = newRouter.matcher // reset router
+    const newRouter = createRouter();
+    router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
--- src/views/modules/article/article.vue
+++ src/views/modules/article/article.vue
@@ -7,17 +7,12 @@
                 搜索
             </el-button>
             <el-button v-if="isAuth('sys:user:save')" class="filter-item" style="margin-left: 10px;" type="primary"
-                icon="el-icon-plus" @click="handleCreate">
+                icon="el-icon-plus" @click="addOrUpdateHandle()">
                 添加
             </el-button>
-            <el-button v-if="isAuth('sys:user:update')" class="filter-item" style="margin-left: 10px;" type="primary"
-                icon="el-icon-edit" @click="handleEdit">
-                修改
-            </el-button>
-            <el-button v-if="isAuth('sys:user:delete')" class="filter-item" style="margin-left: 10px;" type="danger"
-                icon="el-icon-delete" @click="handleDelete">
-                删除
-            </el-button>
+            <el-button type="danger" :disabled="dataListSelections.length <= 0" class="filter-item"
+                @click="deleteHandle()">批量删除</el-button>
+            <el-button type="warning" @click="refreshCache()" class="filter-item">刷新缓存</el-button>
         </div>
 
         <el-table :data="dataList" border v-loading="listLoading" @selection-change="selectionChangeHandle"
@@ -30,7 +25,7 @@
             </el-table-column>
             <el-table-column prop="categoryListStr" header-align="center" align="center" label="分类" width="250">
             </el-table-column>
-            <el-table-column prop="tagList" header-align="center" align="center" label="标签" width="300">
+            <el-table-column prop="tagList" header-align="center" align="center" label="标签" min-width="80">
                 <template slot-scope="scope">
                     <el-row>
                         <el-button v-for="tag in scope.row.tagList" :key="tag.id" size="mini">{{tag.name}}</el-button>
@@ -41,14 +36,14 @@
             </el-table-column>
             <el-table-column prop="likeNum" header-align="center" align="center" width="80" label="喜欢">
             </el-table-column>
-            <el-table-column prop="recommend" header-align="center" align="center" label="推荐">
+            <el-table-column prop="recommend" header-align="center" align="center" width="70" label="推荐">
                 <template slot-scope="scope">
                     <el-switch v-model="scope.row.recommend" active-color="#13ce66"
                         @change="updateRecommend(scope.row.id,scope.row.recommend)">
                     </el-switch>
                 </template>
             </el-table-column>
-            <el-table-column prop="recommend" header-align="center" align="center" label="置顶">
+            <el-table-column prop="recommend" header-align="center" align="center" width="70" label="置顶">
                 <template slot-scope="scope">
                     <el-switch v-model="scope.row.top" active-color="#13ce66"
                         @change="updateTop(scope.row.id,scope.row.top)">
@@ -66,6 +61,12 @@
                     </el-tooltip>
                 </template>
             </el-table-column>
+            <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
+                <template slot-scope="scope">
+                    <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+                    <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">删除</el-button>
+                </template>
+            </el-table-column>
         </el-table>
 
         <pagination v-show="listQuery.total>0" :total="listQuery.total" :page.sync="listQuery.page"
@@ -77,7 +78,7 @@
 import { timeformat } from "@/utils/util";
 import Pagination from "@/components/Pagination"; // secondary package based on el-pagination
 export default {
-    name: "sys-user",
+    name: "article-article",
     components: { Pagination },
     data() {
         return {
@@ -89,7 +90,8 @@ export default {
                 username: ""
             },
             listLoading: false,
-            tableKey: 0
+            tableKey: 0,
+            dataListSelections: []
         };
     },
     created() {
@@ -113,18 +115,99 @@ export default {
                 this.listLoading = false;
             });
         },
-        updatePublish() {},
-        handleCreate() {},
-        handleEdit() {},
-        handleDelete() {},
-        updateRecommend() {},
-        selectionChangeHandle() {},
+        updatePublish() { },
+        addOrUpdateHandle(id) {
+            this.$router.push({ path: 'article/article/update/' + id })
+        },
+        selectionChangeHandle(val) { this.dataListSelections = val },
         dateformat(row) {
             return timeformat(row.createTime);
         },
         changeStatus(data) {
             return data === 1 ? true : false;
-        }
+        },
+        // 刷新缓存
+        refreshCache() {
+            this.$http({
+                url: `/admin/article/cache/refresh`,
+                method: 'delete'
+            }).then(({ data }) => {
+                if (data && data.code === 2000) {
+                    this.$message.success('刷新成功')
+                } else {
+                    this.$message.error(data.message)
+                }
+            })
+        },
+        // 删除
+        deleteHandle(id) {
+            let articleIds = id ? [id] : this.dataListSelections.map(item => {
+                return item.id
+            })
+            this.$confirm(`确定对[id=${articleIds.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                this.$http({
+                    url: '/admin/article/delete',
+                    method: 'delete',
+                    data: this.$http.adornData(articleIds, false)
+                }).then(({ data }) => {
+                    if (data && data.code === 2000) {
+                        this.$message({
+                            message: '操作成功',
+                            type: 'success',
+                            duration: 1500,
+                            onClose: () => {
+                                this.getDataList()
+                            }
+                        })
+                    } else {
+                        this.$message.error(data.message)
+                    }
+                })
+            }).catch(() => { })
+        },
+        // 更新文章推荐状态
+        updateRecommend(id, value) {
+            let data = {
+                id: id,
+                recommend: value
+            }
+            this.updateStatus(data)
+        },
+        // 更新文章推荐状态
+        updateTop(id, value) {
+            let data = {
+                id: id,
+                top: value
+            }
+            this.updateStatus(data)
+        },
+        // 更新文章发布状态
+        updatePublish(id, value) {
+            let data = {
+                id: id,
+                publish: value
+            }
+            this.updateStatus(data)
+        },
+        // 更新文章
+        updateStatus(data) {
+            this.$http({
+                url: `/admin/article/update/status`,
+                method: 'put',
+                data: this.$http.adornData(data)
+            }).then(({ data }) => {
+                if (data && data.code === 2000) {
+                    this.$message.success('更新成功')
+                    this.getDataList()
+                } else {
+                    this.$message.error(data.message)
+                }
+            })
+        },
     }
 };
 </script>
