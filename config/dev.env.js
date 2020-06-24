d33483cbbb475999a4985bb6e5d4ee3d68978ae2
Leeyulong <1097910340@qq.com>
2020-06-24 14:57:33 +0800

    udpate

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    BASE_URL: '"http://localhost:8081/lylblog"'
+    // BASE_URL: '"http://localhost:8081/lylblog"'
+    BASE_URL: '"http://132.232.87.31:8081/lylblog"'
 });
